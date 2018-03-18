8b9b8e5090c3424ad6228e1163484ba955a45786
Adam Chisolm <axchisolm@gmail.com>
2018-03-18 12:01:03 -0500

    Part 1 done

---
+++ app/src/main/java/com/example/adamchisolm/firebasechat2/FriendsActivity.kt
@@ -0,0 +1,70 @@
+package com.example.adamchisolm.firebasechat2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_friends.*
+
+class FriendsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends)
+
+        var ab = getSupportActionBar()
+        ab?.setTitle("FireChat1")
+        ab?.setSubtitle(("Friends List"))
+
+        // To get the back button in the action bar to show and work
+        ab?.setDisplayHomeAsUpEnabled(true)
+
+        if (this.intent.hasExtra("userEmail")) {
+            mUserEmail = this.intent.getStringExtra("userEmail")
+            mUserEmail = this.intent.getStringExtra("userImageUrl")
+        }
+        else {
+            Log.w("debug", "Activity requires a logged in user")
+        }
+
+        attachRecyclerView()
+
+        addCellToRecyclerView(CellData(mUserEmail, mUserImageUrl, "That's me"))
+        addCellToRecyclerView(CellData(mUserEmail, mUserImageUrl, "That's me 2"))
+        addCellToRecyclerView(CellData(mUserEmail, mUserImageUrl, "That's me 3"))
+        addCellToRecyclerView(CellData(mUserEmail, mUserImageUrl, "That's me 4"))
+        addCellToRecyclerView(CellData(mUserEmail, mUserImageUrl, "That's me 5"))
+    }
+
+    private var mUserEmail: String = ""
+    private  var mUserImageUrl: String = ""
+
+
+    // Recycler View stuff
+
+    lateinit var adapter: CellViewAdapter
+
+    private fun attachRecyclerView() {
+        val manager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = manager
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+
+        initializeRecyclerView()
+    }
+
+    private fun initializeRecyclerView() {
+        adapter = CellViewAdapter {view, position -> rowTapped(position)}
+        recyclerView.adapter = adapter
+    }
+
+    private fun rowTapped(position: Int) {
+        Log.d("debug", adapter.rows[position].headerTxt + " " + adapter.rows[position].messageText)
+    }
+
+    private fun addCellToRecyclerView(cellData: CellData) {
+        adapter.addCellData(cellData)
+        recyclerView.smoothScrollToPosition(adapter.getCellCount() - 1)
+    }
+}
